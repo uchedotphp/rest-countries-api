5a7b65336387467424e771c21dc9075fb1d86986
uchedotphp <uchedotphp@gmail.com>
2020-09-02 17:26:17 +0100

    did more tailwind ssetup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./plugins/element.js";
+import "./assets/css/styles.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
