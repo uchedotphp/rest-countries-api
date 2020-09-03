a4471bc9aa81de4b95d42cbf7393dfe00ff23e9f
uchedotphp <uchedotphp@gmail.com>
2020-09-03 13:01:55 +0100

    added  sun and moon svg icons to the project

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bg-darkBlueColor">
     <div class="phones:max-w-screen-desktops flex justify-between mx-auto px-5 py-10">
-      <h4>Where in the world?</h4>
+      <h5 class="phones:text-4xl text-2xl font-extrabold">Where in the world?</h5>
 
-      <button>Dark Mode</button>
+      <button>Light Mode</button>
     </div>
   </div>
 </template>
