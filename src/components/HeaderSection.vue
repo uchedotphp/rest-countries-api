8bfdd3300ceed00ecd36d49c9eee3277fe33ef07
uchedotphp <uchedotphp@gmail.com>
2020-09-03 13:39:45 +0100

    done with HeaderSection.vue

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -3,7 +3,15 @@
     <div class="phones:max-w-screen-desktops flex justify-between mx-auto px-5 py-10">
       <h5 class="phones:text-4xl text-2xl font-extrabold">Where in the world?</h5>
 
-      <button>Light Mode</button>
+      <a href="#" class="text-xl phones:text-2xl font-semibold align-baseline">
+        <img
+          class="inline-block h-6 w-6 align-bottom"
+          src="./../assets/images/sunny-outline.svg"
+          alt
+          srcset
+        />
+        Light Mode
+      </a>
     </div>
   </div>
 </template>
