f37aea53eee2cd6050b682d02e7b9e0e890fd092
uchedotphp <uchedotphp@gmail.com>
2020-09-03 13:43:07 +0100

    made one more adjustment to HeaderSection.vue; added box-shadow to it

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-darkBlueColor">
+  <div class="bg-darkBlueColor shadow-md">
     <div class="phones:max-w-screen-desktops flex justify-between mx-auto px-5 py-10">
       <h5 class="phones:text-4xl text-2xl font-extrabold">Where in the world?</h5>
 
