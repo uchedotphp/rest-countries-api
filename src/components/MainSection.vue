3052f802b0438bfb4ca61412012fe2f234fd2447
uchedotphp <uchedotphp@gmail.com>
2020-09-04 11:07:10 +0100

    pulled in data of countries using axios

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -5,7 +5,26 @@
       <SearchBoxes />
     </div>
     <div class="grid grid-cols-1 phones:grid-cols-4 gap-20 mb-20 container mx-auto w-9/12">
-      <div class="rounded overflow-hidden shadow-lg">
+      <div class="rounded overflow-hidden shadow-lg" v-for="country in countries" :key="country.id">
+        <img class="w-full object-cover h-48" :src="country.flag" :alt="[country.name]" />
+        <div class="px-6 py-8 bg-darkBlueColor">
+          <div class="font-extrabold text-2xl mb-2 py-5">{{country.name}}</div>
+          <p class="text-xl">
+            <span class="font-semibold">Population:</span>
+            {{country.population}}
+          </p>
+          <p class="text-xl">
+            <span class="font-semibold">Region:</span>
+            {{country.region}}
+          </p>
+          <p class="text-xl">
+            <span class="font-semibold">Capital:</span>
+            {{country.capital}}
+          </p>
+        </div>
+      </div>
+
+      <!-- <div class="rounded overflow-hidden shadow-lg">
         <img
           class="w-full object-cover h-48"
           src="https://source.unsplash.com/random"
@@ -103,27 +122,7 @@
             <span class="font-semibold">Capital:</span> Washington, D.C.
           </p>
         </div>
-      </div>
-
-      <div class="rounded overflow-hidden shadow-lg">
-        <img
-          class="w-full object-cover h-48"
-          src="https://source.unsplash.com/random"
-          alt="country flag"
-        />
-        <div class="px-6 py-8 bg-darkBlueColor">
-          <div class="font-extrabold text-2xl mb-2 py-5">United States of America</div>
-          <p class="text-xl">
-            <span class="font-semibold">Population:</span> 323,947,000
-          </p>
-          <p class="text-xl">
-            <span class="font-semibold">Region:</span> Americas
-          </p>
-          <p class="text-xl">
-            <span class="font-semibold">Capital:</span> Washington, D.C.
-          </p>
-        </div>
-      </div>
+      </div>-->
 
       <!-- <div class="px-16 py-10 rounded-lg phones:px-0">
         <img
@@ -234,6 +233,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import HeaderSection from "@/components/HeaderSection";
 import SearchBoxes from "@/components/SearchBoxes";
 export default {
@@ -241,6 +241,22 @@ export default {
   components: {
     HeaderSection,
     SearchBoxes
+  },
+  data() {
+    return {
+      countries: []
+    };
+  },
+  created() {
+    return axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then(({ data }) => {
+        this.countries = data;
+        console.log(data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 };
 </script>
